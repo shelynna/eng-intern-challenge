150d56a73428055edd87f9538e3a007e349a7f4a
unknown <eshelynna@gmail.com>
2024-09-13 07:27:10 +0000

    updated

--- translator.py
+++ translator.py
@@ -13,3 +13,12 @@ ENGLISH_TO_BRAILLE = {
     ',': '..O...', ';': '..OO..', ':': '...O..', '.': '...OO.', '!': '...OO.', '?': '..O.O.'
 }
 
+# used capitalize to signal conversion and nuber sign to signal numbers
+CAPITALIZE = '.....O'  
+NUMBER_SIGN = '....OO'  
+
+# Mapping letters a-j to numbers 1-0 
+NUM_MAPPING = {
+    'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6',
+    'g': '7', 'h': '8', 'i': '9', 'j': '0'
+}
